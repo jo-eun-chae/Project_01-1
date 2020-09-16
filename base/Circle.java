77630d5f19f0bfa1af635eec4c882066ce3d7e57
jo-eun-chae <dmsco9835@naver.com>
2020-09-16 15:34:47 +0900

    make base package & myInterface package

---
+++ base/Circle.java
@@ -0,0 +1,39 @@
+package base;
+
+/**
+ * Circle 클래스
+ *
+ * @author (2019315011 조은채)
+ * @version (2020.09.16)
+ */
+public class Circle
+{
+    private int x; // 원의 x좌표
+    private int y; // 원의 y좌표
+    private int radius; // 원의 반지름
+    
+    /**
+     * Constructor for objects of class Circle
+     */
+    public Circle()
+    {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    /**
+     * 같은 원인지 아닌지 판단하는 메소드
+     *
+     * @param  obj  Circle 메소드에서 만든 객체
+     * @return    true or false
+     */
+    public boolean equals(Object obj)
+    {
+        Circle c = (Circle)obj;
+        if((this.x == c.x) && (this.y == c.y))
+            return true;
+        else
+            return false;
+    }
+}
---
+++ myInterface/Shape.java
@@ -0,0 +1,14 @@
+package myInterface;
+
+/**
+ * Shape 인터페이스
+ *
+ * @author (2019315011 조은채)
+ * @version (2020.09.16)
+ */
+public interface Shape
+{
+    public final double PI = 3.14;
+    public abstract double getArea();
+}
+
